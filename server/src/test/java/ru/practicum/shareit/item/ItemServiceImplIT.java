1754bc762dd1ac5d6e39e560711b67218d09862d
Pavel Usoltsev <usoltsevdisk@yandex.ru>
2025-03-22 18:50:09 +0300

    Удалены неиспользуемые импорты

--- server/src/test/java/ru/practicum/shareit/item/ItemServiceImplIT.java
+++ server/src/test/java/ru/practicum/shareit/item/ItemServiceImplIT.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import ru.practicum.shareit.item.dto.ItemWithBookingAndCommentsDto;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
